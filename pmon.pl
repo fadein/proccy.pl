8591bd218ae1fadbc0055c00eda77c867b5146d4
Erik on RPi <ewfalor@gmail.com>
2016-05-25 10:18:36 -0600

    last update; not well-tested

--- pmon.pl
+++ pmon.pl
@@ -14,24 +14,34 @@ sub ptabHash {
 	return %h;
 }
 
-
+sub logger {
+	print @_;
+	open my $l, '|logger' or return;
+	print $l @_;
+	close $l;
+}
 
 %prev = ptabHash(); @prevPID = sort { $a <=> $b} keys %prev;
 #print Dumper(\%prev), "\n"; exit;
-
 
 sleep SLEEPYTIME;
 while (1) {
 
 	%curr = ptabHash(); @currPID = sort { $a <=> $b} keys %curr;
-	#print "currPID = (@currPID)\nprevPID = (@prevPID)\n";
 
 	my @diff = sdiff(\@prevPID, \@currPID);
 
-	map { print "Process $curr{$_->[1]}->cmdline has appeared\n" }
-		grep { $_->[0] eq "+" } @diff;
+	foreach my $p ( grep { $_->[0] eq '+' } @diff ) {
+		my $c = $curr{$p->[2]};
+		my $cmdl = $c->cmndline;
+		my $when = scalar localtime $c->start;
+		my $exec = $c->exec;
+		my $pid  = $c->pid;
+		my $f = $c->fname;
+		$cmdl =~ s/^$f//;
+		logger( sprintf "[%s] <%5d> '%s'\n", $when, $pid, $exec . $cmdl);
+	}
 
 	%prev = %curr; @prevPID = @currPID;
-	print "\n";
 	sleep SLEEPYTIME;
 }
